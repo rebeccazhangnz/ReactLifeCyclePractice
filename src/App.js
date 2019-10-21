f81df2daa373309121f4e8b9a7c75c6ab869e9d3
Rebecca_Zhang <rebecca.zhang@spark.co.nz>
2019-10-21 20:46:11 +1300

    setup linter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
+import Counter from './components/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Counter />
+            </div>
+        );
+    }
 }
 
 export default App;
