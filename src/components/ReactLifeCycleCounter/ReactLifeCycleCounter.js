00a3d3321d202658985df2c6286a316aab9b1962
Rebecca_Zhang <rebecca.zhang@spark.co.nz>
2019-10-24 19:21:15 +1300

    added life cycle counter component

---
+++ src/components/ReactLifeCycleCounter/ReactLifeCycleCounter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ReactLifeCycleCounter extends React.Component {
+    render() {
+        return <div>this is the Life Cycle counter</div>;
+    }
+}
+
+export default ReactLifeCycleCounter;
