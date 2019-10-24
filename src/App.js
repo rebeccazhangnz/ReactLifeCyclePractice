d954293274572f0cd87384e163d6609888bda63b
Rebecca_Zhang <rebecca.zhang@spark.co.nz>
2019-10-24 19:20:25 +1300

    Added front end routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import Counter from './components/Counter';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import { PageNotFound } from './components/sharedComponent';
+import ReactLifeCycleCounter from './components/ReactLifeCycleCounter';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <Counter />
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={LandingPage} />
+                    <Route exact path="/ReactLifeCycleCounter" component={ReactLifeCycleCounter} />
+                    <Route exact path="/404" component={PageNotFound} />
+                    <Redirect to="/404" />
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
